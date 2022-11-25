c5cf76e35a87fd17856976ca3db3b0d4babf2103
HenriARM <henriarm@yandex.ru>
2022-11-25 12:54:37 +0200

    Upd Folium

--- plot_map.py
+++ plot_map.py
@@ -1,56 +1,62 @@
 import pandas as pd
 import geopandas as gpd
-import folium
 from PIL import Image
 import io
 import imageio
+import glob
 
-# from folium import Choropleth, Circle, Marker
-# from folium.plugins import HeatMap, MarkerCluster
-# import matplotlib.pyplot as plt
-# import webbrowser
-
-f = "./robotex5.csv"
-df = pd.read_csv(f)
-df = df.sort_values(by=["start_time"])
-
-df = df[:100]
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['start_lat'], df['start_lng']))
-print(gdf.info())
-print(gdf.head())
+import folium
+from folium.features import DivIcon
 
 # https://www.latlong.net/place/tallinn-estonia-5882.html
 TALLINN_LAT_LONG = [59.436962, 24.753574]
+CIRCLE_RADIUS = 20
 m = folium.Map(location=TALLINN_LAT_LONG, tiles='openstreetmap', zoom_start=13)
 
-# TODO: print hour by hour (check how to real csv string to datetime)
-#  and generate .gif https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
-# Add circle bubbles to the map
-CIRCLE_RADIUS = 20
-for idx, row in gdf.iterrows():
-    # x - Lat, y - Long
-    # folium.Marker([row['geometry'].x, row['geometry'].y]).add_to(m)
-    folium.Circle(
-        location=[row['geometry'].x, row['geometry'].y],
-        radius=CIRCLE_RADIUS * row['ride_value'],
-        color='green'  # 'red'
-    ).add_to(m)
+f = './robotex5.csv'
+time_col = 'start_time'
+df = pd.read_csv(f, parse_dates=[time_col])
+df = df.sort_values(by=[time_col])
 
-# m.save('map.html')
-img_data = m._to_png(5)
-img = Image.open(io.BytesIO(img_data))
-map_fn = f'map.png'
-img.save(map_fn)
+df = df[:2000]
+df['hour'] = df[time_col].dt.hour
+df['date'] = df[time_col].dt.date
+df['time'] = pd.to_datetime(df['date']) + pd.to_timedelta(df['hour'], unit='h')
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['start_lat'], df['start_lng']))
+gdf = gdf[['time', 'geometry', 'ride_value']]
+print(gdf.info())
+print(gdf.head())
 
-# images = []
-# for filename in filenames:
-#     images.append(imageio.imread(filename))
-# imageio.mimsave('map.gif', images)
+list_of_points = gdf.groupby(['time'])['geometry'].apply(list)
+list_of_vals = gdf.groupby(['time'])['ride_value'].apply(list)
+hours = list_of_points.reset_index()['time'].dt.hour
+days_of_week = list_of_points.reset_index()['time'].dt.day_of_week
 
-# TODO: if point goes out of view?
+for idx in range(len(list_of_points)):
+    print(idx)
+    m = folium.Map(location=TALLINN_LAT_LONG, tiles='openstreetmap', zoom_start=13)
+    for point_idx, point in enumerate(list_of_points[idx]):
+        # Add circle bubbles to the map (x - Lat, y - Long)
+        # folium.Marker([row['geometry'].x, row['geometry'].y]).add_to(m)
+        folium.Circle(
+            location=[point.x, point.y],
+            radius=CIRCLE_RADIUS * list_of_vals[idx][point_idx],
+            color='green'
+        ).add_to(m)
+        folium.map.Marker(
+            TALLINN_LAT_LONG,
+            icon=DivIcon(
+                icon_size=(250, 36),
+                icon_anchor=(0, 0),
+                html=f'<div style="font-size: 20pt">Day {days_of_week[idx]} hour {hours[idx]} </div>',
+            )
+        ).add_to(m)
+    image_data = m._to_png(5)
+    image = Image.open(io.BytesIO(image_data))
+    image.save(f'map_{idx}.png')
 
-# # one option https://www.diva-gis.org/datadown
-# data = gpd.read_file("./EST_adm/EST_adm1.shp")
-# data.plot()
-# plt.show()
-# # and then video https://github.com/bendoesdata/make-a-gif-map-in-python/blob/master/make-a-gif-map/make-a-gif-map.ipynb
+image_filenames = glob.glob('./*.png')
+images = []
+for image_filename in image_filenames:
+    images.append(imageio.imread(image_filename))
+imageio.mimsave('map.gif', images, fps=2)
